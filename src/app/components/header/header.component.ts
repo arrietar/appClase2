1531552510d4e5f21065dfd91b1fe71ddb7a09dd
Rene Arrieta <renearrieta8905@gmail.com>
2023-05-21 08:36:32 -0500

    clase 2

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-header',
+    template: `<h1>Hola amigos del componente</h1>`
+})
+
+export class HeaderComponent{
+
+}
